f792263718bc4f505a713bf833fbeb403a41ad39
Tovics <tovics1@gmail.com>
2019-04-16 16:21:00 +0200

    scripts, css and sample pictures in went to static

--- index2.php
+++ index2.php
@@ -15,8 +15,8 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.css" rel="stylesheet">
-	<link rel="stylesheet" href="/css/lightbox.css" crossorigin="anonymous">
-	<link rel="stylesheet" href="/css/main.css" crossorigin="anonymous">
+	<link rel="stylesheet" href="/static/css/lightbox.css" crossorigin="anonymous">
+	<link rel="stylesheet" href="/static/css/main.css" crossorigin="anonymous">
 
     <title>Hello, world!</title>
   </head>
@@ -112,13 +112,13 @@
 	  </ol>
 	  <div class="carousel-inner">
 		<div class="carousel-item active">
-		  <img class="d-block w-100" src="/img/img1.jpg" alt="First slide">
+		  <img class="d-block w-100" src="/static/img/img1.jpg" alt="First slide">
 		</div>
 		<div class="carousel-item">
-		  <img class="d-block w-100" src="/img/img2.jpg" alt="Second slide">
+		  <img class="d-block w-100" src="/static/img/img2.jpg" alt="Second slide">
 		</div>
 		<div class="carousel-item">
-		  <img class="d-block w-100" src="/img/img3.jpg" alt="Third slide">
+		  <img class="d-block w-100" src="/static/img/img3.jpg" alt="Third slide">
 		</div>
 	  </div>
 	  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -139,7 +139,7 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-	<script src="/js/main.js" crossorigin="anonymous"></script>
+	<script src="/static/js/main.js" crossorigin="anonymous"></script>
     
 	
 
---
+++ static/js/main.js
@@ -0,0 +1,15 @@
+
+
+// MDB Lightbox Init
+
+$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+	
+	
+//carousel script
+
+$('#carouselExampleIndicators').carousel({
+  interval: 300,
+  cycle: true
+}); 
+
+//end carousel script
