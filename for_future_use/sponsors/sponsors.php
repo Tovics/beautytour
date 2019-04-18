9abf6e97155deb71defc42e10b6edcab692fce2e
Tamara Rácz <racztamara97@gmail.com>
2019-04-18 16:57:51 +0200

    carousels added - style tries

--- for_future_use/sponsors/sponsors.php
+++ for_future_use/sponsors/sponsors.php
@@ -14,16 +14,18 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
             integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"
             defer></script>
-    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
             integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"
             defer></script>
-    -->
+
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
             integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"
             defer></script>
-    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js" defer></script>
-    -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ekko-lightbox/5.3.0/ekko-lightbox.min.js" defer></script>
+
+    <script src="static/js/sponsors.js" defer></script>
     <link rel="stylesheet" href="static/css/sponsors.css" crossorigin="anonymous">
+
 </head>
 <body>
 <div class="col-lg-6">
@@ -58,5 +60,118 @@
         </a>
     </div>
 </div>
+
+<div class="container">
+    <div class="row">
+        <div id="carousel" class="carousel slide" data-ride="carousel" data-interval="2000">
+            <ol class="carousel-indicators">
+                <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel" data-slide-to="1"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                        <a href="https://www.google.com" data-toggle="lightbox"
+                           data-gallery="example-gallery">
+                            <img src="static/img/img1.jpg" class="img-fluid">
+                        </a>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                        <a href="static/img/img2.jpg" data-toggle="lightbox"
+                           data-gallery="example-gallery">
+                            <img src="static/img/img2.jpg" class="img-fluid">
+                        </a>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                        <a href="https://www.google.com" data-toggle="lightbox"
+                           data-gallery="example-gallery">
+                            <img src="static/img/img3.jpg" class="img-fluid">
+                        </a>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                        <a href="https://www.google.com" data-toggle="lightbox"
+                           data-gallery="example-gallery">
+                            <img src="static/img/img4.jpg" class="img-fluid">
+                        </a>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                        <a href="https://www.google.com" data-toggle="lightbox"
+                           data-gallery="example-gallery">
+                            <img src="static/img/img5.jpg" class="img-fluid">
+                        </a>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                        <a href="https://www.google.com" data-toggle="lightbox"
+                           data-gallery="example-gallery">
+                            <img src="static/img/img6.jpg" class="img-fluid">
+                        </a>
+                    </div>
+                    <div class="d-none d-lg-block">
+                        <div class="slide-box">
+                            <img src="static/img/img1.jpg" alt="First slide">
+                            <img src="static/img/img2.jpg" alt="First slide">
+                            <img src="static/img/img3.jpg" alt="First slide">
+                            <img src="static/img/img4.jpg" alt="First slide">
+                        </div>
+                    </div>
+                    <div class="d-none d-md-block d-lg-none">
+                        <div class="slide-box">
+                            <img src="static/img/img1.jpg" alt="First slide">
+                            <img src="static/img/img2.jpg" alt="First slide">
+                            <img src="static/img/img3.jpg" alt="First slide">
+                        </div>
+                    </div>
+                    <div class="d-none d-sm-block d-md-none">
+                        <div class="slide-box">
+                            <img src="static/img/img1.jpg" alt="First slide">
+                            <img src="static/img/img2.jpg" alt="First slide">
+                        </div>
+                    </div>
+                    <div class="d-block d-sm-none">
+                        <img src="static/img/img1.jpg" alt="First slide">
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="d-none d-lg-block">
+                        <div class="slide-box">
+                            <img src="static/img/img5.jpg" alt="Second slide">
+                            <img src="static/img/img6.jpg" alt="Second slide">
+                            <img src="static/img/img7.jpg" alt="Second slide">
+                            <img src="static/img/img8.jpg" alt="Second slide">
+                        </div>
+                    </div>
+                    <div class="d-none d-md-block d-lg-none">
+                        <div class="slide-box">
+                            <img src="static/img/img5.jpg" alt="Second slide">
+                            <img src="static/img/img6.jpg" alt="Second slide">
+                            <img src="static/img/img7.jpg" alt="Second slide">
+                        </div>
+                    </div>
+                    <div class="d-none d-sm-block d-md-none">
+                        <div class="slide-box">
+                            <img src="static/img/img5.jpg" alt="Second slide">
+                            <img src="static/img/img6.jpg" alt="Second slide">
+                        </div>
+                    </div>
+                    <div class="d-block d-sm-none">
+                        <img src="static/img/img5.jpg" alt="Second slide">
+                    </div>
+                </div>
+            </div>
+            </div>
+            <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</div>
+
+
+
 </body>
 </html>
