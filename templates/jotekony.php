ddb40785e1dd6631f3fa8ddf2aed668a13ac3442
racz.tamara97 <racztamara97@gmail.com>
2019-04-26 15:09:54 +0200

    Jotekonykodj article created as a new page

---
+++ templates/jotekony.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en" xmlns:v="http://www.w3.org/1999/xhtml" xmlns:o="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="UTF-8">
+    <title>Jótékonykodj!</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <style class="anchorjs"></style>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
+          integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="/static/css/header.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="/static/css/jotekony.css" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"
+            defer></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
+            integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"
+            defer></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
+            integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"
+            defer></script>
+
+</head>
+<body>
+<!-- Ezt minden felhasználásnál meg kell jeleníteni, így ingyenesen használható:
+       # Dual licensed under the MIT or GPL Version 2 licenses. -->
+<?php
+include('header.php');
+?>
+<div class=WordSection1>
+    <div class="cim">
+        <p class=MsoNormal style='margin-bottom:0cm;margin-bottom:.0001pt;line-height:
+            normal;mso-outline-level:3;background:white'><span style='font-size:30pt;
+            font-family:"Oswald",serif;mso-fareast-font-family:"Times New Roman";
+            mso-bidi-font-family:"Times New Roman";color:#242424;mso-fareast-language:HU'>Szépülj
+            és jótékonykodj! - Indul a <span class=SpellE>Beauty</span> Tour<o:p></o:p></span></p>
+    </div>
+    <div class="busz col-3">
+        <img width=315 height=346
+             src="/static/images/GLAMOUR%20BTour%20logo.png" v:shapes="Kép_x0020_1">
+    </div>
+    <br style='mso-ignore:vglayout' clear=ALL>
+    <div class="content col-10">
+        <p class=MsoNormal><span style='font-size:20pt;line-height:107%;font-family:
+"Open Sans",serif;color:#242424;background:white'>Május végétől útjára indítjuk
+az első GLAMOUR <span class=SpellE>Beauty</span> Tour rendezvényünket, hogy tíz
+hétvégén át, az ország tíz különböző plázájában találkozhass a GLAMOUR profi
+szakembereivel. A kijelölt hétvégéken és helyszíneken remek kényeztető
+programokkal várunk. Vásárolj a kijelölt dátumokon és helyszíneken a GLAMOUR <span
+                        class=SpellE>Beautysarokban</span> néhány impozáns kincset, mi pedig a
+bevételből származó teljes összeget a NANE, azaz a&nbsp;</span><span
+                    class=MsoHyperlink><span style='font-size:20pt;line-height:107%;font-family:
+"Open Sans",serif;color:#EC008C;background:white'><a
+                            href="https://www.glamour.hu/monitor/nem-elteti-hanem-elsorvasztja-oket-a-nok-elleni-eroszakrol-beszelgettunk-37037"><span
+                                style='color:#EC008C'>Nők a Nőkért Együtt az Erőszak Ellen Egyesületnek</span></a></span></span><span
+                    style='font-size:20pt;line-height:107%;font-family:"Open Sans",serif;
+color:#242424;background:white'>&nbsp;ajánljuk fel.<o:p></o:p></span></p>
+
+        <p class=MsoNormal>
+            <o:p>&nbsp;</o:p>
+        </p>
+
+        <p class=MsoNormal><span style='font-size:20pt;line-height:107%;font-family:
+"Open Sans",serif;color:#242424;background:white'>Tudtad, hogy Magyarországon
+minden ötödik nő él vagy élt valaha olyan kapcsolatban, ahol partnere
+rendszeresen testileg bántalmazta őt? Hazánkban a családon belüli erőszak
+hetente legalább egy nő életét követeli. Egy nőnek <span class=SpellE>nyolcszor</span>
+annyi esélye van arra, hogy a férje vagy élettársa ölje meg, mint hogy egy
+idegen támadó (rablógyilkos, ismeretlen nemi erőszak elkövető és a többi)
+általi gyilkosság áldozata legyen.&nbsp;</span><span style='font-size:20pt;
+line-height:107%;font-family:"Open Sans",serif;color:#242424'><br>
+<br>
+<span style='background:white'>A NANE, azaz a Nők a Nőkért Együtt az Erőszak
+Ellen Egyesület azzal a céllal jött létre 1994-ben, hogy fellépjenek a nők és a
+gyerekek ellen elkövetett erőszakkal szemben. Az Egyesület közhasznú, <span
+            class=GramE>non-profit</span>, nem-kormányzati szervezet. Az Egyesület egyéni,
+közösségi és társadalmi szinten is tevékenykedik, így mindig szükség van a
+támogatásra.</span></span></p>
+    </div>
+</div>
+
+</body>
+</html>
