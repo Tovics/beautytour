5d616ae348d8d1834c1166e3f3cdedf2c8e4c204
Tamara Rácz <racztamara97@gmail.com>
2019-04-30 14:18:38 +0200

    simple navbar added to pages, css refactor on the images

--- templates/header_1_row.php
+++ templates/header_1_row.php
@@ -1,4 +1,4 @@
-<nav class="navbar sticky-top navbar-expand-lg navbar-dark navbar-custom">
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark navbar-c">
    <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
    </button>
